2348208e53257e7a35b5fc44db72fdd79716de17
jgcaicedo1 <gabrielcaicedo2015@gmail.com>
2024-11-19 21:01:28 -0500

    Creación Estructura Spring Boot
    Se genero la arquitectura inicial.

---
+++ src/test/java/taller1/taller1/Taller1ApplicationTests.java
@@ -0,0 +1,13 @@
+package taller1.taller1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
